cb61ed3170b053771778ae09cdb58f66a136de44
hectorpaez17 <a20200419@utem.edu.mx>
2022-06-27 00:05:04 -0500

    se agrego un list en index y se creo ndc que es el apartado para buscar numero de control de los alumnos

--- index.php
+++ index.php
@@ -1,5 +1,24 @@
 <?php include("resources/views/header.php")?>
 
-<h1>Hola mundo</h1>
+<div class="container my-3">
+    <div >
+        <div >
+            <h3>Bienvenido(a) usuario</h3>
+        </div>
+        <div class="col-xs-12 col-sm-6">
+            <h4>Menú Principal</h4>
+            <ul>
+                <li>
+                    Estadias y egresados.
+                </li>
+                <ul>
+                    <li>
+                        <a href="">Buscador con Número de control</a>
+                    </li>
+                </ul>
+            </ul>
+        </div>
+    </div>
+</div>
 
 <?php include("resources/views/footer.php")?>
---
+++ ndc.php
@@ -0,0 +1,15 @@
+<?php include("resources/views/header.php")?>
+
+<div style="margin-top: 70px; margin-left: 40px;">
+    <div>
+        <div>
+            <h1>Número de control</h1>
+        </div>
+        <div style="margin-top: 10px;">
+           <input type="text" placeholder="Ingresa el número de control" style="width: 250px;">
+           <input type="submit" value="Buscar" style="margin-left: 7px; width: 85px;">
+        </div>
+    </div>
+</div>
+
+<?php include("resources/views/footer.php")?>
