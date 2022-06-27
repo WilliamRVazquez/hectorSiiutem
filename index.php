bdeab44495588f6445a901b4f0860e9998d1d2e7
hectorpaez17 <a20200419@utem.edu.mx>
2022-06-26 19:36:59 -0500

    se hizo la parte del footer

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" style="min-height: 100%; position: relative;">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/resources/css/bootstrap.min.css">
     <title>Titulo</title>
 </head>
-<body>
-    <h1>Hola mundo</h1>
-</body>
-</html>
+<body style="margin: 0; margin-bottom: 40px;">
+<?php
+include("resources/views/footer.php")
+?>
