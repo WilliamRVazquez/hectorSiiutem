4151a0e486723a5aa2951cf5aaffe2aba866fe04
WilliamRVazquez <wvazquez0@ucol.mx>
2022-06-27 22:16:36 -0500

    Se arregló el dropdown del navbar

--- resources/views/header.php
+++ resources/views/header.php
@@ -11,7 +11,7 @@
     <header>
         <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #004152;">
             <div class="container-fluid">
-                <a class="navbar-brand text-white" href="./">SIIUTeM</a>
+                <a class="navbar-brand text-white" href="#">SIIUTeM</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
                 </button>
@@ -32,9 +32,22 @@
                     <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
                         <li><a class="dropdown-item" href="#">Ver Calificaciones</a></li>
                     </ul>
-                </li>
-                    <li class="nav-item">
-                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Extracurriculares
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="#">Inscripción a actividades culturales y deportivas</a></li>
+                    </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Pagos
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="#">Mis pagos referenciados</a></li>
+                    </ul>
                     </li>
                 </ul>
                 <form class="d-flex">
