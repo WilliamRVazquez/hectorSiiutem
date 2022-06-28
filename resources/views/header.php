689b62a0abeee66465c4b0fdd13fa85088661f16
WilliamRVazquez <wvazquez0@ucol.mx>
2022-06-27 23:52:31 -0500

    Se le puso titulo a la pagina

--- resources/views/header.php
+++ resources/views/header.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/resources/css/bootstrap.min.css">
-    <title>Titulo</title>
+    <title>SIIUTeM</title>
 </head>
 <body style="margin: 0; margin-bottom: 40px;">
     <header>
